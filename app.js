7eafeed4c224c587f893719b3e7bbb2cd4a75d88
Ramirez <jramirez1@FVFZMXVMJ1WK.tld>
2022-04-01 10:53:36 -0500

    Get an Element by its ID

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 function run(){
-    alert("Hello World!")
+    document.getElementById("paragraph").innerHTML = "Hello World!";
+
+    document.getElementById("paragraph").style.backgroundColor = "green";
+
+    document.getElementById("paragraph").style.color = "#ffffff"
+
+    document.getElementById("paragraph").style.padding = "20px"
 }
