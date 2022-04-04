a2cac7899d91fe157a6bd3ce575f01c54ac3fb1b
Ramirez <jramirez1@FVFZMXVMJ1WK.tld>
2022-04-04 10:42:50 -0500

    Get a Random Number

--- app.js
+++ app.js
@@ -3,7 +3,27 @@ function run(){
 
     document.getElementById("paragraph").style.backgroundColor = "green";
 
-    document.getElementById("paragraph").style.color = "#ffffff"
+    document.getElementById("paragraph").style.color = "#ffffff";
 
-    document.getElementById("paragraph").style.padding = "20px"
+    document.getElementById("paragraph").style.padding = "20px";
+
+    randomNum();
+}
+
+function randomNum(){
+
+    let ran = Math.floor(Math.random() * 10);
+
+    let x = document.getElementById("random");
+
+    x.innerHTML = ran;
+
+    x.style.backgroundColor = "blue";
+
+    x.style.color = "#ffffff";
+
+    x.style.padding = "20px";
+
+    x.style.textAlign = "center";
+
 }
